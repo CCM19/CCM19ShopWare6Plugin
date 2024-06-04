ef1e54290bd5c16be0cdadcf9230088585b78818
Christoph Grenz <cg@papoo.de>
2024-06-04 19:03:19 +0200

    Reworked integration into Shopware's cookie permission infrastructure
    With this e.g. Google Analytics will correctly be activated on a wider
    range of Shopware versions.

--- papooCcm19Integration6/src/Resources/app/storefront/src/cookie-permission-compat/cookie-permission-compat.plugin.js
+++ papooCcm19Integration6/src/Resources/app/storefront/src/cookie-permission-compat/cookie-permission-compat.plugin.js
@@ -1,11 +1,104 @@
 import CookiePermissionPlugin from 'src/plugin/cookie/cookie-permission.plugin';
+import { COOKIE_CONFIGURATION_UPDATE } from 'src/plugin/cookie/cookie-configuration.plugin';
+import CookieStorage from 'src/helper/storage/cookie-storage.helper';
 
 export default class CookiePermissionCompat extends CookiePermissionPlugin {
     init() {
         if ('CCM' in window) {
-            // do nothing -> disable cookie permission bar
+            // not calling parent init() -> disable cookie permission bar
+            setupShopwareCookieEvents();
         } else {
             super.init();
         }
+    }
+}
+
+function setupShopwareCookieEvents() {
+    // == Cause Shopware cookie consent signals when CCM19 consent changed and set special marker cookies ==
+    var timer = null;
+    var knownCookies = {};
+
+    // Cookie names for which a special marker cookie needs to be set
+    const specialCookies = {
+        '_ga': ['google-analytics-enabled'],
+        '_swag_ga_ga*': ['google-analytics-enabled'],
+        '_gcl_aw': ['google-ads-enabled'],
+        '__gads': ['google-ads-enabled'],
+        '_dc_gtm_UA-*': ['wbm-tagmanager-enabled'],
+        '_dc_gtm_*': ['wbm-tagmanager-enabled'],
+        YSC: ['youtube-video'],
+    };
+
+    // Check which of the marker cookies are already set
+    for (const key in specialCookies) {
+        specialCookies[key].forEach(function (item) {
+            if (CookieStorage.getItem(item) != false) {
+                knownCookies[item] = true;
+            }
+        });
+    }
+
+    function emitShopwareCookieSignal() {
+        timer = null;
+        var activeCookies = {};
+        window.CCM.acceptedCookies.forEach(function (cookieName) {
+            activeCookies[cookieName] = true;
+        });
+
+        // Process special marker cookies
+        for (const key in specialCookies) {
+            if (key in activeCookies) {
+                specialCookies[key].forEach(function (item) {
+                    activeCookies[item] = true;
+                    if (CookieStorage.getItem(item) == false) {
+                        // Set the marker cookie with a short lifetime (~30m)
+                        CookieStorage.setItem(item, '1', 0.02);
+                    }
+                });
+            } else if (key in knownCookies) {
+                specialCookies[key].forEach(function (item) {
+                    if (CookieStorage.getItem(item) !== false) {
+                        // Unset marker cookies, if the matching embedding was disabled
+                        CookieStorage.removeItem(item);
+                    }
+                });
+            }
+        }
+
+        // Set previously set cookies that are no longer active to `false`
+        for (const key in knownCookies) {
+            if (!(key in activeCookies)) {
+                activeCookies[key] = false;
+            }
+        }
+
+        // Compute which cookies have changed
+        var updatedCookies = {};
+        for (const key in activeCookies) {
+            if (activeCookies[key] !== knownCookies[key]) {
+                updatedCookies[key] = (key in activeCookies) ? activeCookies[key] : false;
+            }
+        }
+
+        knownCookies = activeCookies;
+        if (Object.keys(updatedCookies).length > 0) {
+            document.$emitter.publish(COOKIE_CONFIGURATION_UPDATE, updatedCookies);
+        }
+    }
+
+
+    function delayEmitShopwareCookieSignal() {
+        if (timer !== null) {
+            window.clearTimeout(timer);
+        }
+        timer = window.setTimeout(emitShopwareCookieSignal, 30);
+    }
+
+    window.addEventListener('ccm19CookieAccepted', delayEmitShopwareCookieSignal);
+    window.addEventListener('ccm19EmbeddingAccepted', delayEmitShopwareCookieSignal);
+    window.addEventListener('ccm19WidgetClosed', delayEmitShopwareCookieSignal);
+
+    if (window.CCM.consent) {
+        timer = window.setTimeout(emitShopwareCookieSignal, 60);
     }
 }
--- papooCcm19Integration6/src/Resources/app/storefront/src/main.js
+++ papooCcm19Integration6/src/Resources/app/storefront/src/main.js
@@ -1,28 +1,26 @@
 import CookiePermissionCompat from './cookie-permission-compat/cookie-permission-compat.plugin';
 
-import { COOKIE_CONFIGURATION_UPDATE } from 'src/plugin/cookie/cookie-configuration.plugin';
-
+// == Replace standard Shopware Cookie Banner if neccessary  ==
 const PluginManager = window.PluginManager;
 try {
-    PluginManager.override('CookiePermission', CookiePermissionCompat, '[data-cookie-permission]');
-} catch (e) {
+    var pluginIsAsync;
+    try {
+        var pluginData = PluginManager.getPlugin('CookiePermission', true);
+        pluginIsAsync = !('prototype' in pluginData.get('class'));
+    } catch (_ignore) {
+        pluginIsAsync = false;
+    }
+
+    if (pluginIsAsync) {
+        PluginManager.override('CookiePermission',  () => import('./cookie-permission-compat/cookie-permission-compat.plugin'), '[data-cookie-permission]');
+    } else {
+        PluginManager.override('CookiePermission', CookiePermissionCompat, '[data-cookie-permission]');
+    }
+    var data = PluginManager.getPlugin('CookiePermission', true);
+} catch (_ignore) {
     // Could not override CookiePermission plugin -> Banner probably disabled
+    window.console.debug("[CCM19 Plugin] Shopware default cookie banner not enabled -> not integrating into Shopware's CookiePermission infrastructure.")
 }
-
-window.addEventListener('ccm19CookieAccepted', function () {
-    var updatedCookies = {};
-    for (const cookieName of window.CCM.acceptedCookies)
-    {
-        updatedCookies[cookieName] = {
-            cookie: cookieName
-        };
-    }
-    if ('_ga' in updatedCookies) {
-        updatedCookies['google-analytics-enabled'] = updatedCookies['_ga'];
-    }
-
-    document.$emitter.publish(COOKIE_CONFIGURATION_UPDATE, updatedCookies);
-});
 
 // Necessary for the webpack hot module reloading server
 if (module.hot) {
